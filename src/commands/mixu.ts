e8421989d7777f29b8bc53017ea96656ef7f3452
zuzuvelas <zuzuvelas@gmail.com>
2022-01-10 20:08:41 -0700

    fix some lint in mixu

--- src/commands/mixu.ts
+++ src/commands/mixu.ts
@@ -1,15 +1,15 @@
-import { Guild } from 'discord.js';
-import { Discord, SimpleCommand, SimpleCommandMessage } from 'discordx';
+import { Guild } from 'discord.js'
+import { Discord, SimpleCommand, SimpleCommandMessage } from 'discordx'
 
 @Discord()
 abstract class Mixu {
-  private numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  private bestScore = 0;
-  private bestMixuTiles: number[] = [];
-  private mixuChannel = '340275382093611011';
+  private numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  private bestScore = 0
+  private bestMixuTiles: number[] = []
+  private mixuChannel = '340275382093611011'
 
   private shuffle(): number[] {
-    return [...this.numbers].sort(() => 0.5 - Math.random());
+    return [...this.numbers].sort(() => 0.5 - Math.random())
   }
 
   private score(tiles: number[]): number {
@@ -18,10 +18,10 @@ abstract class Mixu {
     // if it is followed by the correct tile
     // if it has the correct under it
     // https://discord.com/channels/103678524375699456/340275382093611011/341046054952632330
-    let score = 0;
+    let score = 0
     for (const [i, tile] of tiles.entries()) {
       if (i === tile) {
-        score++;
+        score++
       }
       if (
         i % 4 != 3 && // not the last tile in the row
@@ -29,64 +29,60 @@ abstract class Mixu {
         i + 1 < tiles.length &&
         tiles[i + 1] - tile === 1
       ) {
-        score++;
+        score++
       }
       if (i + 4 < 16 && tiles[i + 4] - tile === 4) {
-        score++;
+        score++
       }
     }
-    return score;
+    return score
   }
 
   private stringify(tiles: number[], guild: Guild): string {
     // Find emote corresponding to the tile
     return tiles
       .map((t, i) => {
-        const emote = guild.emojis.cache.find((e) => e.name === `miku${t + 1}`)?.toString();
-        return i % 4 === 0 ? '\n' + emote : emote;
+        const emote = guild.emojis.cache.find((e) => e.name === `miku${t + 1}`)?.toString()
+        return i % 4 === 0 ? '\n' + emote : emote
       })
-      .join('');
+      .join('')
   }
 
   private isMixuChannel(channel: string): boolean {
-    return channel === this.mixuChannel;
+    return channel === this.mixuChannel
   }
 
   @SimpleCommand('mixu', { directMessage: false })
   mixu(command: SimpleCommandMessage) {
-    if (!command.message.guild || !this.isMixuChannel(command.message.channel.id)) return;
+    if (!command.message.guild || !this.isMixuChannel(command.message.channel.id)) return
 
-    const tiles = this.shuffle();
-    const score = this.score(tiles);
+    const tiles = this.shuffle()
+    const score = this.score(tiles)
 
     if (score > this.bestScore) {
-      this.bestScore = score;
-      this.bestMixuTiles = tiles;
+      this.bestScore = score
+      this.bestMixuTiles = tiles
     }
 
-    const text = this.stringify(tiles, command.message.guild);
-    command.message.channel.send(
-      `:regional_indicator_m::regional_indicator_i::regional_indicator_x::regional_indicator_u:${text}`,
-    );
+    const text = this.stringify(tiles, command.message.guild)
+    command.message.channel.send(`:regional_indicator_m::regional_indicator_i::regional_indicator_x::regional_indicator_u:${text}`)
   }
 
   @SimpleCommand('bestmixu', { directMessage: false })
   bestMixu(command: SimpleCommandMessage) {
     if (!command.message.guild || !this.isMixuChannel(command.message.channel.id) || this.bestMixuTiles.length === 0) {
-      return;
+      return
     }
 
-    const text = this.stringify(this.bestMixuTiles, command.message.guild);
-    command.message.channel.send(`${text}`);
+    const text = this.stringify(this.bestMixuTiles, command.message.guild)
+    command.message.channel.send(`${text}`)
   }
 
   @SimpleCommand('mikustare', { directMessage: false })
   mikustare(command: SimpleCommandMessage) {
-    if (!command.message.guild || !this.isMixuChannel(command.message.channel.id)) return;
+    if (!command.message.guild || !this.isMixuChannel(command.message.channel.id)) return
 
-    const text = this.stringify(this.numbers, command.message.guild);
-    command.message.channel.send(
-      `:regional_indicator_m::regional_indicator_i::regional_indicator_k::regional_indicator_u:${text}`,
-    );
+    const text = this.stringify(this.numbers, command.message.guild)
+    command.message.channel.send(`:regional_indicator_m::regional_indicator_i::regional_indicator_k::regional_indicator_u:${text}`)
   }
 }
