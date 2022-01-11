631c5ec790916e6d187810c06032c1d7c6d9154f
zuzuvelas <zuzuvelas@gmail.com>
2022-01-10 19:11:59 -0700

    linter changes, add build folder to gitignore and remove starter repo commands

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
-  semi: true,
-  trailingComma: 'all',
+  semi: false,
+  trailingComma: 'es5',
   singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2,
+  printWidth: 150,
+  tabWidth: 2
 };
