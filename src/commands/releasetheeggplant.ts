353cc8a257936c5c976fd1abd113c397e6712167
Jordon Shields <jordon.shields@cmcflex.com>
2022-01-12 09:29:55 -0700

    Fix the eggplant command... we all learned something today 😌

--- src/commands/releasetheeggplant.ts
+++ src/commands/releasetheeggplant.ts
@@ -1,31 +1,33 @@
-import { CommandInteraction } from 'discord.js'
-import { ArgsOf, Discord, SimpleCommand, SimpleCommandOption, SimpleCommandMessage, SlashOption, Slash } from 'discordx'
+import { CommandInteraction, User } from 'discord.js'
+import { Discord, SimpleCommand, SimpleCommandMessage, SimpleCommandOption, Slash, SlashOption } from 'discordx'
 
 @Discord()
 class ReleaseTheEggplant {
-
-  @SimpleCommand('releasetheeggplant', { description: 'Release the eggplant on a user of your choosing', argSplitter: '\n'})
-  simple(@SimpleCommandOption("name", { type: "USER" }) name: user | undefined, command: SimpleCommandMessage) {
-    if (!name) return command.message.reply("usage: ``>releasetheeggplant <user>``");
-    let originalNickname = command.message.client.user?.username;
-    let botId = command.message.client.user?.id;
-    let thisBot = command.message.guild.members.cache.find(u => u.id === botId);
-    thisBot.setNickname("🍆🔪");
-    command.message.channel.send(`I'm coming for you, ${name}!`);
-    setTimeout(() => thisBot.setNickname(null), 30000);
+  @SimpleCommand('releasetheeggplant', {
+    description: 'Release the eggplant on a user of your choosing',
+    argSplitter: '\n',
+  })
+  simple(@SimpleCommandOption('name') name: User, command: SimpleCommandMessage) {
+    if (!name) return command.message.reply('usage: ``>releasetheeggplant <user>``')
+    let botId = command.message.client.user?.id
+    let thisBot = command.message.guild?.members.cache.find((u) => u.id === botId)
+    thisBot?.setNickname('🍆🔪')
+    command.message.channel.send(`I'm coming for you, ${name}!`).then((_) => {
+      setTimeout(() => thisBot?.setNickname(null), 5000)
+    })
   }
 
   @Slash('releasetheeggplant', { description: 'Release the eggplant on a user of your choosing' })
   async slash(
-    @SlashOption('name', { type: "USER", required: true })
-    name: user,
+    @SlashOption('name')
+    name: User,
     interaction: CommandInteraction
   ) {
-    let originalNickname = command.message.client.user?.username;
-    let botId = command.message.client.user?.id;
-    let thisBot = command.message.guild.members.cache.find(u => u.id === botId);
-    thisBot.setNickname("🍆🔪");
-    interaction.channel.send(`I'm coming for you, ${name}!`);
-    setTimeout(() => thisBot.setNickname(null), 30000);
+    let botId = interaction.channel?.client.user?.id
+    let thisBot = interaction.guild?.members.cache.find((u) => u.id === botId)
+    thisBot?.setNickname('🍆🔪')
+    interaction.reply(`I'm coming for you, ${name}!`).then((_) => {
+      setTimeout(() => thisBot?.setNickname(null), 5000)
+    })
   }
 }
