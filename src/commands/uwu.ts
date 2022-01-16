373965b284b623f8e7001d47459047913eba432a
Martin Merfort <mmerfort@users.noreply.github.com>
2022-01-16 12:06:38 +0100

    Refactor uwu function
    Move the uwu function out of the class and export it. Simplify it a bit.

--- src/commands/uwu.ts
+++ src/commands/uwu.ts
@@ -1,33 +1,25 @@
-import { FormattingPatterns } from 'discord-api-types'
-import { CommandInteraction, TextBasedChannel, TextChannel } from 'discord.js'
-import { Discord, SimpleCommand, SimpleCommandMessage, SlashOption, Slash, SimpleCommandOption } from 'discordx'
+import { Discord, SimpleCommand, SimpleCommandMessage, SimpleCommandOption } from 'discordx'
 
+export const uwuify = (text: string): string => {
+  /* Each pattern is a tuple containing a search pattern and its associated replacement string */
+  var patterns: [RegExp,string][] = [
+    [ RegExp("r|l", "g"),              "w"     ],
+    [ RegExp("R|L", "g"),              "W"     ],
+    [ RegExp("n([aeiouAEIOU])", "g"),  "ny$1"  ],
+    [ RegExp("N([aeiou])/", "g"),      "Ny$1"  ],
+    [ RegExp("N([AEIOU])/", "g"),      "NY$1"  ]
+  ]
+
+  /* Iterate over each pattern and replace it in the user input string */
+  patterns.forEach(([re, replacement]) => {
+    text = text.replaceAll(re, replacement)
+  })
+
+  return text
+}
 
 @Discord()
 class UwU {
-
-  private uwuify(text: string): string {
-    
-    var pattern_index:number
-
-    /* Each pattern is a tuple containing a search pattern and its associated replacement string */
-    var patterns:[RegExp,string][] = [
-      [ RegExp("r|l", "g"),              "w"     ],
-      [ RegExp("R|L", "g"),              "W"     ],
-      [ RegExp("n([aeiouAEIOU])", "g"),  "ny$1"  ],
-      [ RegExp("N([aeiou])/", "g"),      "Ny$1"  ],
-      [ RegExp("N([AEIOU])/", "g"),      "NY$1"  ]
-    ]
-
-    /* Iterate over each pattern and replace it in the user input string */
-    for (pattern_index = 0; pattern_index < patterns.length; pattern_index++)
-    {
-      text = text.replaceAll(patterns[pattern_index][0], patterns[pattern_index][1])
-    }
-
-    return text
-  }
-
   @SimpleCommand('uwu', { description: 'UwUify text', argSplitter: '\n' })
   simple(@SimpleCommandOption('text', { type: 'STRING' }) text: string | undefined, command: SimpleCommandMessage) {
     
@@ -35,6 +27,6 @@ class UwU {
       return command.sendUsageSyntax()
     }
 
-    command.message.channel.send(this.uwuify(text))
+    command.message.channel.send(uwuify(text))
   }
 }
