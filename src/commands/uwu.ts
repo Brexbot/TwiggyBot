08143b577e513fccaf1a8552417f0502cee74342
Martin Merfort <mmerfort@users.noreply.github.com>
2022-01-16 12:37:11 +0100

    Simplify regexp handling and add slash command to uwu

--- src/commands/uwu.ts
+++ src/commands/uwu.ts
@@ -1,18 +1,20 @@
-import { Discord, SimpleCommand, SimpleCommandMessage, SimpleCommandOption } from 'discordx'
+import { CommandInteraction } from 'discord.js'
+import { Discord, SimpleCommand, SimpleCommandMessage, SimpleCommandOption, Slash, SlashOption } from 'discordx'
 
 export const uwuify = (text: string): string => {
-  /* Each pattern is a tuple containing a search pattern and its associated replacement string */
-  var patterns: [RegExp,string][] = [
-    [ RegExp("r|l", "g"),              "w"     ],
-    [ RegExp("R|L", "g"),              "W"     ],
-    [ RegExp("n([aeiouAEIOU])", "g"),  "ny$1"  ],
-    [ RegExp("N([aeiou])/", "g"),      "Ny$1"  ],
-    [ RegExp("N([AEIOU])/", "g"),      "NY$1"  ]
+  // Each pattern is a tuple containing a search pattern and its associated replacement string
+  const patterns: [RegExp, string][] = [
+    [/r|l/g, 'w'],
+    [/R|L/g, 'W'],
+    [/n([aeiouAEIOU])/g, 'ny$1'],
+    [/N([aeiou])/g, 'Ny$1'],
+    [/N([AEIOU])/g, 'NY$1'],
+    [/ove/g, 'uv']
   ]
 
-  /* Iterate over each pattern and replace it in the user input string */
+  // Iterate over each pattern and replace it in the user input string
   patterns.forEach(([re, replacement]) => {
-    text = text.replaceAll(re, replacement)
+    text = text.replace(re, replacement)
   })
 
   return text
@@ -21,12 +23,25 @@ export const uwuify = (text: string): string => {
 @Discord()
 class UwU {
   @SimpleCommand('uwu', { description: 'UwUify text', argSplitter: '\n' })
-  simple(@SimpleCommandOption('text', { type: 'STRING' }) text: string | undefined, command: SimpleCommandMessage) {
-    
+  async simple(
+    @SimpleCommandOption('text', { type: 'STRING' }) text: string | undefined,
+    command: SimpleCommandMessage
+  ) {
     if (!text) {
       return command.sendUsageSyntax()
     }
 
-    command.message.channel.send(uwuify(text))
+    await command.message.channel.send(uwuify(text))
+  }
+
+  @Slash('uwu', { description: 'UwUify text' })
+  private async slash(
+    @SlashOption('text', { type: 'STRING' })
+    text: string,
+    interaction: CommandInteraction
+  ) {
+    await interaction.deferReply()
+
+    await interaction.followUp(uwuify(text))
   }
 }
