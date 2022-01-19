c6552a981af5d3d9b4a022d3e62bce17b9005dd3
riccardofano <rcr.fano@gmail.com>
2022-01-19 12:57:01 +0100

    Add timeout command

--- src/commands/timeout.ts
+++ src/commands/timeout.ts
@@ -1,9 +1,20 @@
-import { CommandInteraction, Guild, GuildMember, GuildMemberRoleManager } from 'discord.js'
-import { Discord, SimpleCommand, SimpleCommandMessage, Slash } from 'discordx'
+import { CommandInteraction, Guild, GuildMember, GuildMemberRoleManager, User } from 'discord.js'
+import { Discord, SimpleCommand, SimpleCommandMessage, SimpleCommandOption, Slash } from 'discordx'
 
 @Discord()
 abstract class Timeout {
-  hasPermission(command: SimpleCommandMessage | CommandInteraction): boolean {
+  // TODO: import roles from global config
+  private modRoles = [
+    '103679575694774272', // Brex Mods
+    '104750975268483072', // Brex scum
+  ]
+  private gozId = '104819134017118208'
+
+  hasModPowers(member: GuildMember | null): boolean {
+    return member?.roles.cache.some((role) => this.modRoles.includes(role.id)) ?? false
+  }
+
+  hasPermission(command: SimpleCommandMessage | CommandInteraction, target?: GuildMember): boolean {
     let guild: Guild | null
     let memberRoles: GuildMemberRoleManager | undefined
     let userId: string
@@ -20,6 +31,11 @@ abstract class Timeout {
         return false
       }
       memberRoles = _memberRoles
+    }
+
+    if (target) {
+      memberRoles = target.roles
+      userId = target.user.id
     }
 
     const highestBotRole = guild?.me?.roles.highest
@@ -58,5 +74,30 @@ abstract class Timeout {
     const time = this.sudokuDuration()
     await interaction.member.timeout(time * 1000, "Sudoku'd")
     await interaction.reply(`${interaction.user}, you're timed out for ${time} seconds.`)
+  }
+
+  @SimpleCommand('timeout')
+  async timeoutCommand(
+    @SimpleCommandOption('user', { type: 'USER' }) user: GuildMember | User | undefined,
+    @SimpleCommandOption('duration', { type: 'NUMBER' }) duration: number | undefined,
+    command: SimpleCommandMessage
+  ) {
+    if (
+      !(user instanceof GuildMember) ||
+      !this.hasModPowers(command.message.member) ||
+      !this.hasPermission(command, user)
+    ) {
+      return
+    }
+
+    if (!duration) {
+      await command.message.channel.send('Duration has to be a number.')
+      return
+    }
+
+    await user.timeout(duration * 1000)
+    if (command.message.author.id === this.gozId) {
+      await command.message.channel.send('In the name of the Moon, I shall punish you!')
+    }
   }
 }
