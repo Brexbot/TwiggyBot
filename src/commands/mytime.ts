69a958288aff9e08bc181b9feb78127abbaf57f9
QOAL <quiche_on_a_leash@hotmail.co.uk>
2022-02-21 19:23:10 +0000

    Weather & MyTime improvements
    Weather:
    Adds help command
    Fixes places without a country (Such as just doing /weather europe)
    MyTime:
    Updates to the latest module
    Introduces embeds for error messages
    Escapes urls in localised messages
    Fixes simple command usage

--- src/commands/mytime.ts
+++ src/commands/mytime.ts
@@ -1,30 +1,39 @@
-import { CommandInteraction } from 'discord.js'
+import { CommandInteraction, MessageEmbed, ColorResolvable } from 'discord.js'
 import { Discord, SimpleCommand, SimpleCommandMessage, SimpleCommandOption, Slash, SlashOption } from 'discordx'
 import { localiseInput } from 'localisetimemodule'
+
+const embedColors: Array<ColorResolvable> = ["#30cb9c", "#cf3463"]
+const makeSafeRegEx = /(:\/\/)/g
+function makeSafe(text: string): string {
+    return text.replace(makeSafeRegEx, '\\$1')
+}
 
 @Discord()
 class MyTime {
 
-  @SimpleCommand('mytime', { description: 'Localised times within the provided text' })
+  @SimpleCommand('mytime', { description: 'Localised times within the provided text', argSplitter: "\t" })
   async simple(
     @SimpleCommandOption('text', {
       type: 'STRING',
       description: 'Text containing times',
     })
     text: string,
-    @SimpleCommandOption('mode', {
-      type: 'STRING',
-      description: 'The output mode. One of: tTdDfFR, Default: t',
-    })
-    mode: string,
-    @SimpleCommandOption('raw', {
-      type: 'BOOLEAN',
-      description: 'Outputs the time in a copy/paste friendly format for discord ',
-    })
-    raw: boolean,
     command: SimpleCommandMessage
   ) {
-    await command.message.reply({content: localiseInput(text, mode, raw), allowedMentions: { repliedUser: false } } )
+    const localisedInfo = localiseInput(text)
+
+    if (localisedInfo[1]) {
+      command.message.channel.send({ content: makeSafe(localisedInfo[0]) })
+    } else {
+      command.message.channel.send({ embeds: [
+        new MessageEmbed()
+        .setAuthor({
+          name: 'MyTime'
+        })
+        .setColor(embedColors[1])
+        .setDescription(this.formatError(localisedInfo[0]))
+      ] })
+    }
   }
 
   @Slash('mytime', { description: 'Localised times within the provided text' })
@@ -48,7 +57,30 @@ class MyTime {
     raw: boolean,
     interaction: CommandInteraction
   ) {
-    const localisedTimes = localiseInput(text, mode, raw)
-    interaction.reply({ content: localisedTimes, ephemeral: localisedTimes === "🤷" })
+    const localisedInfo = localiseInput(text, mode, raw)
+
+    if (localisedInfo[1]) {
+      interaction.reply({ content: makeSafe(localisedInfo[0]) })
+    } else {
+      interaction.reply({ embeds: [
+        new MessageEmbed()
+        .setAuthor({
+          name: 'MyTime'
+        })
+        .setColor(embedColors[1])
+        .setDescription(this.formatError(localisedInfo[0]))
+      ], ephemeral: true })
+    }
+  }
+
+  private formatError(errorText: string): string {
+    switch (errorText) {
+      case "noInput":
+        return "🤔 The input seems empty."
+      case "noTimesDetected":
+        return "🤷‍♀️ No times detected in the input."
+      default:
+        return "🤯 What did you do?!"
+    }
   }
 }
