30967bcbd960a69865c3db86b8263008cdcaeecc
JFurness1 <james.w.furness.1@gmail.com>
2022-03-12 15:43:47 -0500

    Reduced code duplication in random functions

--- src/commands/RPG/util.ts
+++ src/commands/RPG/util.ts
@@ -22,12 +22,12 @@ export function mulberry32(a: number): () => number {
   }
 }
 
-export function getRandomElement(a: Array<any>) {
+export function getRandomElement(a: any[]) {
   // returns a random element from a using Math.random()
-  return a[Math.floor(Math.random() * a.length)]
+  return getSeededRandomElement(a, Math.random)
 }
 
-export function getSeededRandomElement(a: Array<any>, rng: () => number) {
+export function getSeededRandomElement(a: any[], rng: () => number) {
   // returns a random element from a,
   // calling rng argument to get random number.
   return a[Math.floor(rng() * a.length)]
@@ -47,12 +47,8 @@ export function rollSeeded_dy_x_TimesPick_z(sides: number, total: number, pick: 
 export function roll_dy_x_TimesPick_z(sides: number, total: number, pick: number): number {
   // Roll {total} d{sides} dice, and sum the top {pick} results
   // random generation is done by unseeded Math.random()
-  const rolls = []
-  for (let i = 0; i < total; i++) {
-    rolls.push(Math.floor(Math.random() * sides + 1))
-  }
-  rolls.sort().reverse()
-  return rolls.slice(0, pick).reduce((a, b) => a + b, 0)
+
+  return rollSeeded_dy_x_TimesPick_z(sides, total, pick, Math.random)
 }
 
 export function getEloRankChange(rankA: number, rankB: number, K: number, result: 'win' | 'loss' | 'draw'): number {
