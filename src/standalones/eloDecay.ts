f4f41c3ebb4cbba9facc21e085adcd4f702223ae
JFurness1 <james.w.furness.1@gmail.com>
2022-03-27 22:18:27 -0400

    Elo drift ballancing added

--- src/standalones/eloDecay.ts
+++ src/standalones/eloDecay.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from '../../../prisma/generated/prisma-client-js'
-import { ELO_DECAY_FACTOR } from './Data'
+import { PrismaClient, RPGCharacter } from '../../prisma/generated/prisma-client-js'
+import { ELO_DECAY_FACTOR } from '../commands/RPG/Data'
+import { getRandomElement } from '../commands/RPG/util'
 
 function calculateEloDecay(eloRank: number): number {
   // Linear interpolation between eloRank and 1000 which is the mean ladder "skill"
@@ -9,6 +10,7 @@ function calculateEloDecay(eloRank: number): number {
 }
 
 async function main() {
+  console.log(`\nElo Decay running at ${new Date()}`)
   const client = new PrismaClient()
 
   const allEntries = await client.rPGCharacter.findMany()
@@ -33,7 +35,42 @@ async function main() {
       },
     })
   }
+
+  // Due to rounding, points can get lost or created during the adjustment
+  // Over time this would cause the "average skill" to move away from 1000
+  // So we have tallied up this "Elo drift" to make a pool of missing points
+  // Sort it by Elo rank, allowing us to take points from the rich, and give them
+  // to the poor. As required to keep the status quo.
+
+  const postDecayAllEntries = await client.rPGCharacter.findMany()
   console.log(`Final drift ${eloDrift}`)
+  const mod = Math.sign(eloDrift)
+  const filterdEntries = postDecayAllEntries.filter((a: RPGCharacter) => Math.sign(a.eloRank - 1000) == mod)
+  while (eloDrift != 0) {
+    const randomEntry = getRandomElement(filterdEntries)
+    const newEloRank = randomEntry.eloRank - mod
+    console.log(`Assigning ${-mod} to ${randomEntry.id} was ${randomEntry.eloRank} now ${newEloRank}.`)
+    await client.rPGCharacter.update({
+      where: {
+        id: randomEntry.id,
+      },
+      data: {
+        eloRank: newEloRank,
+        // This time the Elo decay fairy may gift/grift someone a point and push
+        // them to a new extreme value. We must always update this.
+        peakElo: Math.max(randomEntry.peakElo, newEloRank),
+        floorElo: Math.min(randomEntry.floorElo, newEloRank),
+      },
+    })
+    eloDrift -= mod
+  }
+
+  const postFixAllEntries = await client.rPGCharacter.findMany()
+  let tally = 0
+  for (const i in postFixAllEntries) {
+    tally += postFixAllEntries[i].eloRank
+  }
+  console.log(`Mean Elo now ${tally / postFixAllEntries.length}`)
 }
 
 main()
