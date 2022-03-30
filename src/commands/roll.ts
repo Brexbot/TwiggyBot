4fed81802611be21da5fe63cdea084e10d228ee1
JFurness1 <james.w.furness.1@gmail.com>
2022-03-30 12:58:38 -0400

    Fixed abusable output lengths

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -4,6 +4,9 @@ import { Discord, SimpleCommand, SimpleCommandMessage, SlashOption, Slash, Simpl
 
 @Discord()
 class Roll {
+  private MAX_REPLY_LENGTH = 200
+  private MAX_MESSAGE_LENGTH = 150
+
   @Slash('roll', { description: 'Roll some dice' })
   async slash(
     @SlashOption('dice', { type: 'STRING' })
@@ -12,22 +15,44 @@ class Roll {
     message: string,
     interaction: CommandInteraction
   ) {
+    let roll: DiceRoll
     try {
-      const roll = new DiceRoll(dice)
-      let preamble: string
-      if (!message) {
-        preamble = ''
-      } else {
-        preamble = '(' + message + ') '
-      }
-      await interaction.reply(preamble + roll.toString())
+      roll = new DiceRoll(dice)
     } catch (e) {
+      console.log(e)
       await interaction.reply({
         content: `I don't understand those dice.\nTake a look here for help: https://dice-roller.github.io/documentation/guide/notation/`,
         ephemeral: true,
       })
-
       return
+    }
+    let preamble: string
+    if (!message) {
+      preamble = ''
+    } else {
+      if (message.length > this.MAX_MESSAGE_LENGTH) {
+        await interaction.reply({
+          content: `Your message must be < ${this.MAX_MESSAGE_LENGTH} characters.`,
+          ephemeral: true,
+        })
+      }
+      preamble = '(' + message + ') '
+    }
+
+    // The output of roll.rolls is somewhat complicated with the more exotic rolling
+    // options. So, just check the message length for simplicity.
+    let diceOut = roll.toString()
+    if (diceOut.length <= this.MAX_REPLY_LENGTH - preamble.length) {
+      diceOut = roll.toString()
+    } else {
+      diceOut = `${roll.notation}: [Too many to list] = ${roll.total}`
+    }
+    const final = preamble + diceOut
+
+    if (final.length <= this.MAX_REPLY_LENGTH) {
+      await interaction.reply(final)
+    } else {
+      await interaction.reply({ content: 'The output was too large to send...', ephemeral: true })
     }
   }
 }
