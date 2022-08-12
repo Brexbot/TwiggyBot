26b1ea9e16f2ccd230b79b7d80d3aa89d2ba1812
James Furness <6610047+JFurness1@users.noreply.github.com>
2022-08-12 16:38:44 -0400

    Soft-max price cap
    This makes the price asymptote rather than clip at the maximum. I did it in github editor so I can't test or lint it. :rexShy:

--- src/commands/NFD.ts
+++ src/commands/NFD.ts
@@ -795,7 +795,8 @@ class NFD {
 
   private getNFDPrice(nfd: NFDItem): number {
     // Stupid little function to make an NFD more "valuable" the more times it has been traded, with a bit of drift
-    return 2 ** Math.min(nfd.previousOwners.split(',').length - 1 + Math.random(), this.MAX_NFD_PRICE_EXPONENT)
+    const exponent = Math.tanh(nfd.previousOwners.split(',').length - 1 + Math.random()/this.MAX_NFD_PRICE_EXPONENT)*this.MAX_NFD_PRICE_EXPONENT
+    return 2 ** exponent
   }
 
   private async ensureImageExists(fileName: string, name: string, code: string) {
