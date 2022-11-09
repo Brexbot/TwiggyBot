ce86957a77e2bef6f6e05afe8090a5a0b90c7119
JFurness1 <james.w.furness.1@gmail.com>
2022-11-08 20:24:23 -0500

    Added cooldown

--- src/events/Tuturu.ts
+++ src/events/Tuturu.ts
@@ -9,11 +9,16 @@ import * as path from 'path'
 @Discord()
 abstract class Tuturu {
   private imagePath = path.join(__dirname, '../assets/Tuturu.png')
+  private cooldown = 60 // 1 minute
+  private lastToot = 0 // Initializes most recent time tuturu was tooted
 
   @On('messageCreate')
   private onMessage([message]: ArgsOf<'messageCreate'>) {
-    if (message.content.toLowerCase().includes('tuturu')) {
-      console.log(this.imagePath)
+    if (
+      Math.floor(Date.now()) - this.lastToot > this.cooldown * 1000 &&
+      message.content.toLowerCase().includes('tuturu')
+    ) {
+      this.lastToot = Math.floor(Date.now())
       const imageAttachment = new AttachmentBuilder(this.imagePath)
       message.channel.send({ files: imageAttachment ? [imageAttachment] : [] })
     }
