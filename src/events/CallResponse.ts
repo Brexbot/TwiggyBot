e99f31397fce001fc810eec70a99e260098e2f47
Jordon Shields <jordon.shields@cmcflex.com>
2022-11-11 08:31:42 -0700

    Triggers are now regex to make them more verbose and clean up some logic

--- src/events/CallResponse.ts
+++ src/events/CallResponse.ts
@@ -4,57 +4,55 @@ import { Discord, On } from 'discordx'
 import * as path from 'path'
 
 // Class for simple call/response type things
-
 type CallResponse = {
-  call: string
+  expression: RegExp
   response?: string
   responseAttachment?: string
   cooldown: number // seconds
   lastUse: number
-  caseSensitive: boolean
 }
 
 @Discord()
 abstract class CallAndResponder {
+  private BasePath = '../../src/assets'
+
+  // todo: Move these into a json obj and serialize and cache the obj at runtime
+  //  Would allow us to add triggers via a mod command should we choose to
+
   // Array of call/response conditions.
   // Note that the order in the array reflects priority.
   // Bot will only respond to one trigger, and that will be the first in the array.
   // Not the first in the message.
   private calls: CallResponse[] = [
     {
-      call: 'tuturu',
-      responseAttachment: path.join(__dirname, '../../src/assets/Tuturu.png'),
+      expression: /\btuturu\b/i,
+      responseAttachment: 'Tuturu.png',
       cooldown: 60,
       lastUse: 0,
-      caseSensitive: false,
     },
     {
-      call: 'zuzuru',
-      responseAttachment: path.join(__dirname, '../../src/assets/Zuzuru.png'),
+      expression: /zuzuru/i,
+      responseAttachment: 'Zuzuru.png',
       cooldown: 60,
       lastUse: 0,
-      caseSensitive: false,
     },
   ]
 
   @On('messageCreate')
   private onMessage([message]: ArgsOf<'messageCreate'>) {
     const prompt = message.content
-    const lcPrompt = prompt.toLowerCase()
     for (const call of this.calls) {
       if (
-        // Check timeout first
-        Math.floor(Date.now()) - call.lastUse > call.cooldown * 1000 &&
-        // If caseSensitive look for call exactly
-        ((call.caseSensitive && prompt.includes(call.call)) ||
-          // Otherwise convert to lowercase and search
-          (!call.caseSensitive && lcPrompt.includes(call.call.toLowerCase())))
+        // Message triggers a response?
+        call.expression.test(prompt) &&
+        // And this response isn't on cooldown
+        Math.floor(Date.now()) - call.lastUse > call.cooldown * 1000
       ) {
         call.lastUse = Math.floor(Date.now())
 
         let imageAttachment: AttachmentBuilder[]
         if (call.responseAttachment) {
-          imageAttachment = [new AttachmentBuilder(call.responseAttachment)]
+          imageAttachment = [new AttachmentBuilder(path.join(__dirname, `${this.BasePath}/${call.responseAttachment}`))]
         } else {
           imageAttachment = []
         }
