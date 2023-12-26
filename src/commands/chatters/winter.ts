e98d0e9e719988c46a2765e7622407637adbe7f1
Yusu <alexgmin+githubyusu@gmail.com>
2023-12-26 16:33:30 +0100

    Added !winter command

---
+++ src/commands/chatters/winter.ts
@@ -0,0 +1,22 @@
+import { Discord, SimpleCommand, SimpleCommandMessage, SimpleCommandOption, SimpleCommandOptionType } from 'discordx'
+
+@Discord()
+class Winter {
+  @SimpleCommand({ name: 'winter', description: 'Winter', prefix: '!', argSplitter: '\n' })
+  async simple(
+    @SimpleCommandOption({ name: 'text', type: SimpleCommandOptionType.String }) text: string | undefined,
+    command: SimpleCommandMessage
+  ) {
+    let content
+    if (text !== undefined) {
+      content = `${text}? Estrogen can help with that`
+    } else {
+      content = 'Estrogen can help with that'
+    }
+
+    await command.message.channel.send({
+      content: content,
+    })
+    return
+  }
+}
